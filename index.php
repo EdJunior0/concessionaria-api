44f5fb9d2915b4e43fd2905c73bd98d143fdb08d
Edson Júnior <edsonjunior.1219@gmail.com>
2022-06-22 21:39:28 -0300

    refact: change url

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ use src\Router;
 require_once 'vendor/autoload.php';
 
 $url = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+var_dump($url);
 $method = $_SERVER['REQUEST_METHOD'];
 $json = file_get_contents('php://input');
 header('Content-type: application/json');
-$router = new Router($url[1], json_decode($json, true), $method);
+$router = new Router($url[3], json_decode($json, true), $method);
 $router->start();
