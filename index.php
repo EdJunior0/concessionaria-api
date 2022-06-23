1fdc0ba07ef6a2c6df8d9d58a97b859f11d756d1
Edson Júnior <edsonjunior.1219@gmail.com>
2022-06-22 23:41:31 -0300

    change

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ use src\Router;
 require_once 'vendor/autoload.php';
 
 $url = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-var_dump($_SERVER['REQUEST_URI']);
 $method = $_SERVER['REQUEST_METHOD'];
 $json = file_get_contents('php://input');
 header('Content-type: application/json');
--- src/config/Connection.php
+++ src/config/Connection.php
@@ -12,10 +12,16 @@ class Connection
 
   public static function getConn()
   {
+    $HOST = getenv("HOST");
+    $PORT = getenv("PORT");
+    $DB_NAME = getenv("DB_NAME");
+    $BD_USER = getenv("BD_USER");
+    $DB_PASSWORD = getenv("DB_PASSWORD");
+    $CONN = "pgsql:host=" . $HOST . ";port=" . $PORT . ";dbname=" . $DB_NAME;
     if (!isset(self::$instance)) :
-      self::$instance = new PDO("pgsql:host=localhost;port=5432;dbname=concessionaria", "postgres", "123456");
+      self::$instance = new PDO($CONN, $BD_USER, $DB_PASSWORD);
     endif;
-    
+
     return self::$instance;
   }
 }
