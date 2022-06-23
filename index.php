8fd5d1dd40a791b9919a11d78279b2eb0297dae1
Edson Júnior <edsonjunior.1219@gmail.com>
2022-06-22 22:46:53 -0300

    change var dump

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ require_once 'vendor/autoload.php';
 
 $url = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 var_dump($_SERVER['REQUEST_URI']);
-$method = $_SERVER['REQUEST_METHOD'];
-$json = file_get_contents('php://input');
-header('Content-type: application/json');
-$router = new Router($url[1], json_decode($json, true), $method);
-$router->start();
+// $method = $_SERVER['REQUEST_METHOD'];
+// $json = file_get_contents('php://input');
+// header('Content-type: application/json');
+// $router = new Router($url[1], json_decode($json, true), $method);
+// $router->start();
