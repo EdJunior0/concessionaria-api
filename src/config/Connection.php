c48b100c1021a74497436e446ba595d0ff4ba391
Edson Júnior <edsonjunior.1219@gmail.com>
2022-07-10 14:05:16 -0300

    changes

--- src/config/Connection.php
+++ src/config/Connection.php
@@ -12,10 +12,11 @@ class Connection
 
   public static function getConn()
   {
-    $HOST = "ec2-52-72-56-59.compute-1.amazonaws.com";
-    $DB_NAME = "d98ctljod9kohm";
-    $BD_USER = "mykgrcdtvwbtbo";
-    $DB_PASSWORD = "8a04b2a2fa76648b42115498d343cb988cc6a8451c9b62b851e6e62e55186ed8";
+    $HOST = getenv("HOST");
+    echo $HOST;
+    $DB_NAME = getenv("DB_NAME");
+    $BD_USER = getenv("DB_USER");
+    $DB_PASSWORD = getenv("DB_PASSWORD");
     $CONN = "pgsql:host=$HOST;port=5432;dbname=$DB_NAME";
     if (!isset(self::$instance)) :
       self::$instance = new PDO($CONN, $BD_USER, $DB_PASSWORD);
